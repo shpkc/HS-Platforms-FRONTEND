4f646f28b1dfc42a6561e968c9a689fec654378b
Park Si Hyung <shpkc@naver.com>
2022-03-25 01:31:58 +0900

    chore : adding main items

--- src/components/organisms/home/HomeContents.tsx
+++ src/components/organisms/home/HomeContents.tsx
@@ -3,42 +3,57 @@ import { AxiosResponse } from "axios";
 import HsContainer from "src/components/atoms/layout/HsContainer";
 import HsText from "src/components/atoms/text/HsText";
 import HomeBanner from "./homeComponents/HomeBanner";
+import HsImage from "src/components/atoms/image/HsImage";
 
 const HomeContents = ({ data }: { data: AxiosResponse["data"] }) => {
-	const [currentPage, setCurrentPage] = React.useState(0);
-
+	console.log(data);
 	return (
 		<HsContainer padding={"61px 0 0 0"}>
 			<HsContainer margin={["0 0 60px 0", "0 0 100px 0"]}>
 				<HomeBanner data={data.banner} />
 			</HsContainer>
-			<HsContainer width={[, "1100px"]} margin={[, "0 auto"]}>
+			<HsContainer
+				width={[, "1100px"]}
+				margin={[, "0 auto"]}
+				padding={"0 16px"}
+			>
 				<HsContainer
-					padding="50px 0"
+					padding={["30px 6px", "50px 0"]}
 					backgroundColor="#F8F9FA"
-					margin="0 0 100px 0"
+					margin={["0 0 50px 0", "0 0 100px 0"]}
 				>
 					<HsText
 						textAlign={"center"}
 						fontWeight="bold"
-						fontSize={"1.5rem"}
+						fontSize={["1.2rem", "1.5rem"]}
 						margin="0 0 15px 0"
 					>
 						Start Your NFT Trade Here
 					</HsText>
-					<HsText textAlign={"center"} margin="0 0 10px 0" fontSize={"1.2rem"}>
+					<HsText
+						textAlign={"center"}
+						margin="0 0 10px 0"
+						fontSize={["1rem", "1.5rem"]}
+						lineHeight={"20px"}
+					>
 						You can create and transfer your own extraordinary NFTs
 					</HsText>
 				</HsContainer>
 				<HsContainer>
-					<HsText variant="h4" margin="0 0 30px 0">
-						Recommend NFTs
+					<HsText variant="h4" margin="0 0 50px 0">
+						Explore
 					</HsText>
-				</HsContainer>
-				<HsContainer>
-					<HsText variant="h4" margin="0 0 30px 0">
-						Recommend Collections
-					</HsText>
+					{data.recommend.map(item => (
+						<HsContainer key={item.id} width={[, "280px"]}>
+							<HsContainer width={[, "300px"]} height={[, "300px"]}>
+								<HsImage
+									src={item.image}
+									borderRadius="4px"
+									objectFit="cover"
+								/>
+							</HsContainer>
+						</HsContainer>
+					))}
 				</HsContainer>
 			</HsContainer>
 		</HsContainer>
