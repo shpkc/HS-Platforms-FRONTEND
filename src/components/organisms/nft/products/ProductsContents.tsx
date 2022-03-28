b77638e2f93feead1c05da91fe855008e81304bf
Park Si Hyung <shpkc@naver.com>
2022-03-29 01:54:18 +0900

    chore : adding product page

--- src/components/organisms/nft/products/ProductsContents.tsx
+++ src/components/organisms/nft/products/ProductsContents.tsx
@@ -5,12 +5,13 @@ import HsText from "src/components/atoms/text/HsText";
 const ProductsContents = ({ data }) => {
 	console.log(data);
 	return (
-		<HsContainer
-			padding={["80px 16px 0 16px", "140px 0 0 0"]}
-			width={["100%", "1200px"]}
-			margin="0 auto"
-		>
-			<HsContainer display={["block", "flex"]} justifyContent="space-between">
+		<HsContainer padding={["80px 16px 0 16px", "140px 0 0 0"]}>
+			<HsContainer
+				display={["block", "flex"]}
+				justifyContent="space-between"
+				width={["100%", "1200px"]}
+				margin="0 auto"
+			>
 				<HsContainer width={[, "384px"]}>
 					<HsContainer display={["block", "none"]}>
 						<HsText variant="h3">{data.title}</HsText>
@@ -22,10 +23,15 @@ const ProductsContents = ({ data }) => {
 							objectFit={"cover"}
 						/>
 					</HsContainer>
-					<HsText fontWeight={"500"} fontSize={"1.2rem"} margin="0 0 5px 0">
+					<HsText fontWeight={"500"} fontSize={"1.2rem"} margin="0 0 10px 0">
 						Description
 					</HsText>
-					<HsText color={"gray.gray20"}>{data.description}</HsText>
+					<HsText color={"gray.gray30"} lineHeight={1.2} margin="0 0 30px 0">
+						{data.description}
+					</HsText>
+					<HsText fontWeight={"500"} fontSize={"1.2rem"} margin="0 0 10px 0">
+						Details
+					</HsText>
 				</HsContainer>
 				<HsContainer width={[, "720px"]}>
 					<HsText variant="h3">{data.title}</HsText>
