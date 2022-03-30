108c5889582e100c72bebb00792c23272edb1d82
Park Si Hyung <shpkc@naver.com>
2022-03-30 17:46:17 +0900

    chore : adding history table

--- src/apis/ContractApi.ts
+++ src/apis/ContractApi.ts
@@ -3,25 +3,30 @@ import { ethers } from "ethers";
 import NFTMarketplace from "../../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json";
 
 // NOTE : NFT 구매 web3 api
-export const purchaseNFT = async ({
+export const purchaseProductWithWeb3 = async ({
 	price,
 	tokenId,
 }: {
 	price: number;
 	tokenId: string;
 }) => {
-	const web3Modal = new Web3Modal();
-	const connection = await web3Modal.connect();
-	const provider = new ethers.providers.Web3Provider(connection);
-	const signer = provider.getSigner();
-	const contract = new ethers.Contract(
-		process.env.NEXT_PUBLIC_NFT_MARKET_PLACE_ADDRESS,
-		NFTMarketplace.abi,
-		signer
-	);
-	const convertedPrice = ethers.utils.parseUnits(price.toString(), "ether");
-	const transaction = await contract.createMarketSale(tokenId, {
-		value: convertedPrice,
-	});
-	await transaction.wait();
+	try {
+		const web3Modal = new Web3Modal();
+		const connection = await web3Modal.connect();
+		const provider = new ethers.providers.Web3Provider(connection);
+		const signer = provider.getSigner();
+		const contract = new ethers.Contract(
+			process.env.NEXT_PUBLIC_NFT_MARKET_PLACE_ADDRESS,
+			NFTMarketplace.abi,
+			signer
+		);
+		const convertedPrice = ethers.utils.parseUnits(price.toString(), "ether");
+		const transaction = await contract.createMarketSale(tokenId, {
+			value: convertedPrice,
+		});
+		await transaction.wait();
+		// NOTE : purchase history add
+	} catch (e) {
+		console.log(e);
+	}
 };
