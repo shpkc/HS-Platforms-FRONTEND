bb823a81250aa4bdacaa4615b63386122b2312f9
Park Si Hyung <shpkc@naver.com>
2022-06-06 23:15:49 +0900

    chore : add ga4

--- apps/blog/src/pages/_document.tsx
+++ apps/blog/src/pages/_document.tsx
@@ -1,10 +1,30 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
+declare global {
+	interface Window {
+		dataLayer: any;
+	}
+}
 export default class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="ko">
 				<Head>
+					<script
+						async
+						src="https://www.googletagmanager.com/gtag/js?id=G-4180VKWTVQ"
+					></script>
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+     				window.dataLayer = window.dataLayer || [];
+        			function gtag(){dataLayer.push(arguments);}
+        			gtag('js', new Date());
+        			gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          			page_path: window.location.pathname,
+        			});
+      				`,
+						}}
+					/>
 					<link rel="shortcut icon" href="/static/icons/favicon.ico" />
 					<link
 						href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap"
